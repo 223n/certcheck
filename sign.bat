f80e6e644eaac59768a62e3176f235ebfc315b49
223n <223n@223n.tech>
2018-10-11 11:06:58 +0900

    add go build

--- sign.bat
+++ sign.bat
@@ -1,2 +1,9 @@
-"C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe" sign /v /a /tr http://timestamp.globalsign.com/?signature=sha2 /td sha256 /fd sha256 certcheck_windows_amd64.exe
-"C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe" sign /v /a /tr http://timestamp.globalsign.com/?signature=sha2 /td sha256 /fd sha256 certcheck_windows_386.exe
+set GOOS=windows
+set GOARCH=386
+go build -o certcheck_windows_386.exe
+go build -o certcheck_windows_386_sign.exe
+set GOARCH=amd64
+go build -o certcheck_windows_amd64.exe
+go build -o certcheck_windows_amd64_sign.exe
+"C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe" sign /v /a /tr http://timestamp.globalsign.com/?signature=sha2 /td sha256 /fd sha256 certcheck_windows_amd64_sign.exe
+"C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe" sign /v /a /tr http://timestamp.globalsign.com/?signature=sha2 /td sha256 /fd sha256 certcheck_windows_386_sign.exe
